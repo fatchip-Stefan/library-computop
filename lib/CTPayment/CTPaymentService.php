17bc0d31319e4350d239453a698fd7a71a9fb1c8
Stefan v.d.B <stefan.vanden.berg@gmail.com>
2018-02-28 15:21:40 +0100

    Removed IssuerList for Sofort

--- lib/CTPayment/CTPaymentService.php
+++ lib/CTPayment/CTPaymentService.php
@@ -276,7 +276,7 @@ class CTPaymentService extends Blowfish
                 'shortname' => 'SOFORT',
                 'description' => 'Computop SOFORT',
                 'action' => 'FatchipCTSofort',
-                'template' => 'fatchip_computop_sofort.tpl',
+                'template' => '',
                 'additionalDescription' => '',
                 'className' => 'Sofort',
             ],
